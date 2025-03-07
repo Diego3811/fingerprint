34646692219e0ad6971930f133f31ac6dfaf0892
Diego <125624090+Diego3811@users.noreply.github.com>
2025-03-07 15:43:13 -0600

    Scanner Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FingerprintscanerComponent } from './fingerprintscaner/fingerprintscaner.component'; // Asegúrate de tener esta importación
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [RouterOutlet, FingerprintscanerComponent],
+  template: `
+    <h1>{{ title }}</h1>
+    <router-outlet></router-outlet>
+    <app-fingerprintscaner></app-fingerprintscaner>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
